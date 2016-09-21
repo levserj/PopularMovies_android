c5e828da431edfdd41270849d062e5a51fa31cf7
levserj <levserj@yahoo.com>
2016-09-21 22:49:11 +0300

    - Some cleaning

--- app/src/main/java/com/example/sd/popularmovies/SettingsActivity.java
+++ app/src/main/java/com/example/sd/popularmovies/SettingsActivity.java
@@ -2,19 +2,13 @@ package com.example.sd.popularmovies;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.preference.ListPreference;
-import android.preference.Preference;
-import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
 
-import java.util.List;
 
 /**
  * Created by SD on 15.09.2016.
  */
-public class SettingsActivity extends Activity
-        /*implements Preference.OnPreferenceChangeListener*/ {
+public class SettingsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,40 +27,4 @@ public class SettingsActivity extends Activity
             addPreferencesFromResource(R.xml.pref_general);
         }
     }
-
-    /**
-     * Attaches a listener so the summary is always updated with the preference value.
-     * Also fires the listener once, to initialize the summary (so it shows up before the value
-     * is changed.)
-     */
-/*    private void bindPreferenceSummaryToValue(Preference preference) {
-        // Set the listener to watch for value changes.
-        preference.setOnPreferenceChangeListener(this);
-
-        // Trigger the listener immediately with the preference's
-        // current value.
-        onPreferenceChange(preference,
-                PreferenceManager
-                        .getDefaultSharedPreferences(preference.getContext())
-                        .getString(preference.getKey(), ""));
-    }*/
-
-/*    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        String stringValue = newValue.toString();
-
-        if (preference instanceof ListPreference) {
-            // For list preferences, look up the correct display value in
-            // the preference's 'entries' list (since they have separate labels/values).
-            ListPreference listPreference = (ListPreference) preference;
-            int prefIndex = listPreference.findIndexOfValue(stringValue);
-            if (prefIndex >= 0) {
-                preference.setSummary(listPreference.getEntries()[prefIndex]);
-            }
-        } else {
-            // For other preferences, set the summary to the value's simple string representation.
-            preference.setSummary(stringValue);
-        }
-        return true;
-    }*/
 }
