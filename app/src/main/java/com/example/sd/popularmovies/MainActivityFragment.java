c5db5e432f000f6b2d5eec5bb3ffbcfa7a0c76dc
levserj <levserj@yahoo.com>
2016-09-26 12:18:34 +0300

    - Two buttons Next&Previous page added    Previous button is hidden on first page

--- app/src/main/java/com/example/sd/popularmovies/MainActivityFragment.java
+++ app/src/main/java/com/example/sd/popularmovies/MainActivityFragment.java
@@ -21,6 +21,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.sd.popularmovies.model.Movie;
@@ -52,6 +53,7 @@ public class MainActivityFragment extends Fragment {
         setHasOptionsMenu(true);
         PreferenceManager.setDefaultValues(getContext(), R.xml.pref_general, false);
         pref = PreferenceManager.getDefaultSharedPreferences(getContext());
+        /*pref.edit().putInt("page", 1).apply();*/
     }
 
     @Override
@@ -90,19 +92,48 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        FloatingActionButton forward = (FloatingActionButton) rootView.findViewById(R.id.fab);
-        forward.setImageDrawable(getResources().getDrawable(R.drawable.forward));
+
+        /*FloatingActionButton forward = (FloatingActionButton) rootView.findViewById(R.id.fab);
+        forward.setImageDrawable(getResources().getDrawable(R.drawable.next));
         forward.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int nextPage = Integer.parseInt(pref.getString(getString(R.string.pref_page_key),
-                        getString(R.string.pref_page_def_value))) + 1;
-                pref.edit().putString(getString(R.string.pref_page_key), String.valueOf(nextPage)).apply();
+                int nextPage = pref.getInt("page", 1)
+                        *//*Integer.parseInt(pref.getString(getString(R.string.pref_page_key),
+                        getString(R.string.pref_page_def_value))) *//*+ 1;
+                pref.edit().putInt("page", nextPage).apply(); *//*putString(getString(R.string.pref_page_key), String.valueOf(nextPage)).apply()*//*
+                updateMovies();
+            }
+        });*/
+        int currentPage = pref.getInt("page", 1);
+        Log.v(LOG_TAG, String.valueOf(currentPage));
+        Log.v(LOG_TAG, (String.valueOf(currentPage == 1)));
+        ImageButton previous = (ImageButton) rootView.findViewById(R.id.button_previous);
+
+        previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int previousPage = pref.getInt("page", 1) - 1;
+                Log.v(LOG_TAG, String.valueOf(previousPage));
+                pref.edit().putInt("page", previousPage).apply();
+                updateMovies();
+            }
+        });
+        ImageButton next = (ImageButton) rootView.findViewById(R.id.button_next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int nextPage = pref.getInt("page", 1) + 1;
+                Log.v(LOG_TAG, String.valueOf(nextPage));
+                pref.edit().putInt("page", nextPage).apply();
                 updateMovies();
             }
         });
         GridView gridView = (GridView) rootView.findViewById(R.id.gridView);
         movieArrayAdapter = new MovieArrayAdapter(getContext(), new ArrayList<Movie>());
+        movieArrayAdapter.notifyDataSetChanged();
         gridView.setAdapter(movieArrayAdapter);
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -121,18 +152,30 @@ public class MainActivityFragment extends Fragment {
         return netInfo != null && netInfo.isConnected();
     }
 
+    public void showOrHidePreviousButton(int currentPage){
+        ImageButton previous = (ImageButton) getView().findViewById(R.id.button_previous);
+        if (currentPage == 1){
+            previous.setVisibility(View.GONE);
+
+        } else {
+            previous.setVisibility(View.VISIBLE);
+        }
+    }
+
     public class FetchMovies extends AsyncTask<String, Void, List<Movie>> {
         List<Movie> moviesList = new ArrayList<>();
         List<MovieDb> movieDBList = new ArrayList<>();
-        int page = Integer.parseInt(pref.getString(getString(R.string.pref_page_key),
-                getString(R.string.pref_page_def_value)));
+        int page = pref.getInt("page", 1); /*Integer.parseInt(pref.getString(getString(R.string.pref_page_key),
+                getString(R.string.pref_page_def_value)));*/
         String language = pref.getString(getString(R.string.pref_language_key),
                 getString(R.string.pref_language_value_ru));
         String sorting = pref.getString(getString(R.string.pref_sorting_key),
                 getString(R.string.pref_sorting_value_popular));
 
+
         @Override
         protected List<Movie> doInBackground(String... params) {
+
             TmdbMovies movies = new TmdbApi("91ca123680c7da4ae30a546026abae71").getMovies();
 
             if (sorting.equals(getString(R.string.pref_sorting_value_popular))) {
@@ -154,6 +197,9 @@ public class MainActivityFragment extends Fragment {
                 for (Movie movie : movieList)
                     movieArrayAdapter.add(movie);
             }
+            showOrHidePreviousButton(page);
+
+
         }
 
     }
